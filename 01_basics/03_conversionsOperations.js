7cb4ea956ca838c859e9bc2d8b758d1983cd1c5e
Taiba Zaheer <taibazaheer5@gmail.com>
2026-04-25 14:32:24 +0530

    lecture 6

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -18,7 +18,7 @@ let valueInNumber = Number (score) // guranteed it is a number
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 //1 => true; 0 => false
@@ -29,5 +29,31 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber); //string
+// console.log(stringNumber)
+// console.log(typeof stringNumber); //string
+
+
+//***********Operation *************//
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log("1" + 2) //Output = 12
+// console.log(1 + "2") //Output = 12
+// console.log("1" + 2 + 2) //Output = 122
+// console.log(1 + 2 + "2") //Output = 32 
+//Not a good (above) way to write a code
+
+// Do not do that
+// console.log(+true) // "+" do the conversion
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// POST-INCREMENT(x++) : use x then add 1
+// PRE-INCREMENT(++x) : add 1 then use x
+
+
+
